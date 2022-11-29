eef0c85b2cf2a8d14b8e5597af27982728365c7f
olalekan <bhadmuslekan@gmail.com>
2022-11-29 14:02:37 -0800

    Print Combo

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(0, 100):
-    if i <= 9:
-        print("0{}".format((i)), end=", ")
-    elif i > 9 and i <= 98:
-        print("{}".format((i)), end=", ")
+    if i <= 98:
+        print("{0:02d}".format((i)), end=", ")
     else:
         print("99")
