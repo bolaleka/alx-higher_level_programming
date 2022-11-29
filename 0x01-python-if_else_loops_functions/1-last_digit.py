55c1937cb45177a8e8b0690414273de7cfdb824e
olalekan <bhadmuslekan@gmail.com>
2022-11-29 10:00:25 -0800

    last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 import random
 
-n = random.randint(-10000, 10000)
-if n > 0:
-    print(f"Last digit of {n} is {n % 10} ", end="")
-    if n % 10 > 5:
+number = random.randint(-10000, 10000)
+if number > 0:
+    print(f"Last digit of {number} is {number % 10} ", end="")
+    if number % 10 > 5:
         print(f"and is greater than 5")
-    elif n % 10 <= 5 and n % 10 > 0:
+    elif number % 10 <= 5 and number % 10 > 0:
         print(f"and is less than 6 and not 0")
-    elif n % 10 == 0:
+    elif number % 10 == 0:
         print(f"and is 0")
-elif n < 0:
-    if n % 10 == 0:
-        print(f"Last digit of {n} is {n % 10} ", end="")
+elif number < 0:
+    if number % 10 == 0:
+        print(f"Last digit of {number} is {number % 10} ", end="")
     else:
-        print(f"Last digit of {n} is -{n % 10} ", end="")
-    if n % 10 != 0:
+        print(f"Last digit of {number} is -{number % 10} ", end="")
+    if number % 10 != 0:
         print(f"and is less than 6 and not 0")
-    elif n % 10 == 0:
+    elif number % 10 == 0:
         print(f"and is 0")
-elif n == 0:
+elif number == 0:
     print(f"Last digit of 0 is 0 and is 0")
 else:
     print(f"TypeError")
