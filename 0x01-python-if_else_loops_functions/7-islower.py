aa44ad162be44864965eb15a6eef07169141fd4c
olalekan <bhadmuslekan@gmail.com>
2022-11-30 01:30:10 -0800

    is lower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def islower(c):
+    if c == '':
+        return "Traceback (most recent call last):"
     if c != chr(97) and c != chr(123):
         return False
     return True
