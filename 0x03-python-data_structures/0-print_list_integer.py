f9aee9d736daf2626af5eda6910b08dba561669d
olalekan <bhadmuslekan@gmail.com>
2022-12-05 00:14:39 -0800

    print list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    res = '\n'.join(str(item) for item in my_list)
-    print(res);
+    for i in my_list:
+        print("{}".format(i), end='\n')
