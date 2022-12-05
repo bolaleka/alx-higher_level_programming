71f5f0e2ec6166debf2406d6d4d238c65f548b78
olalekan <bhadmuslekan@gmail.com>
2022-12-05 02:47:19 -0800

    Print isLower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def islower(c):
-    if c >= chr(97) and c <= chr(123):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
-    elif c == '"':
-        return False
     return False
