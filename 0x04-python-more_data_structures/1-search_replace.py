ab7a69da27158762ce6a36caf5034158314476be
olalekan <bhadmuslekan@gmail.com>
2022-12-07 06:35:08 -0800

    More Data Structure

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    res = [replace if i == search else i for i in my_list]
+    res = [replace if i == search else i
+        for i in my_list]
     return res
     
