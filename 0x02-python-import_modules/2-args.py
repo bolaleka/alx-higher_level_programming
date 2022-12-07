1d2d1ef675faecc15997748b57bbb2f11e4acec8
olalekan <bhadmuslekan@gmail.com>
2022-12-06 23:53:01 -0800

    Print Program Argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-def add_arg(argv):
+def print_arg(argv):
     n = len(argv) - 1
     if n == 0:
-        print("{:d}".format(n))
+        print("{:d} argument.".format(n))
         return
     else:
+        if n == 1:
+            print("{:d} argument:".format(n))
+        else:
+            print("{:d} arguments:".format(n))
         i = 1
-        add = 0
         while i <= n:
-            add += int(argv[i])
+            print("{:d}: {:s}".format(i, argv[i]))
             i += 1
-        print("{:d}".format(add))
 
 if __name__ == "__main__":
     import sys
-    add_arg(sys.argv)
+    print_arg(sys.argv)
