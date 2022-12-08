d379eb952f4ad658d47df9e4eea715788b97fce9
olalekan <bhadmuslekan@gmail.com>
2022-12-08 00:14:30 -0800

    More Data Structure

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,7 +3,5 @@
 def common_elements(set_1, set_2):
     set1 = set(set_1)
     set2 = set(set_2)
-    if (set1 & set2):
+    if (set1 & set2) is not None:
         return set1 & set2
-    else:
-        return
