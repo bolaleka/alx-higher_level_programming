b73cccfb467cf945b123a1e5ff54d83adaa6e135
olalekan <bhadmuslekan@gmail.com>
2022-12-08 04:41:54 -0800

    More Data Structure

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    new_set = set_1.union(set_2)
-    return new_set
+    list_1 = list(set_1)
+    list_2 = list(set_2)
+    new_list = []
+    for j in list_1:
+        if j not in list_2:
+            new_list.append(j)
+    for i in list_2:
+        if i not in list_1:
+            new_list.append(i)
+    return new_list
