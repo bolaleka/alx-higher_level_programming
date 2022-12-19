7fef137cf31e11f2f94de5a7175d22d9655393cf
olalekan <bhadmuslekan@gmail.com>
2022-12-19 14:16:07 -0800

    Python - Exceptions

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    res = 0
+    try:
+        res = a / b
+    except:
+        pass
+    finally:
+        if res != 0:
+            print("Inside result: {}".format(res))
+        else:
+            print("Inside result: None")
+    return res
