04e9535482f2d05fa14c8afb62ef96becb39b732
olalekan <bhadmuslekan@gmail.com>
2022-12-19 23:33:48 -0800

    Python - Exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,5 +4,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except IndexError:
-        return False
+    except ValueError:
+        pass
+    return False
