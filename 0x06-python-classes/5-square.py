dc1d07b288715a978cbef0106346375e7fcdf226
olalekan <bhadmuslekan@gmail.com>
2022-12-21 11:07:54 -0800

    ython - Classes and Objectswq

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Create class Square by checking an exception"""
+
+
+class Square:
+
+    """Create init method"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    """Create getter method with private instance"""
+    @property
+    def size(self):
+        return self.__size
+
+    """Create setter method with private instance"""
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.size is 0:
+            print("")
+        else:
+            for i in range(self.size)
+                [print("#" end="") for i in range(self.zie)]
+                print("")
