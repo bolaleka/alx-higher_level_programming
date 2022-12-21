5a3209a1937f45437a5cb8839ebf8e1f25060aaf
olalekan <bhadmuslekan@gmail.com>
2022-12-21 01:04:40 -0800

    Python - Classes and Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Create a class Square with Private
+   instance attribute: size
+"""
+
+
+class Square:
+    """ Method to create square size"""
+
+    def __init__(self, size):
+        self.__size = size
