43f55c3a30bbae0c6ab5090d000897a83f2e3dd3
olalekan <bhadmuslekan@gmail.com>
2022-12-21 00:57:20 -0800

    Python - Classes and Objects

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
-"""This class is to create an empty class
+"""This class is to create an empty class"""
 
-"""
 
 class  Square:
 
-    """Do nothing
-
-    """
+    """Do nothing"""
     pass
