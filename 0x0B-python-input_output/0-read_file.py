d7368af010d0988eb43936f9711e8112afe5fb05
olalekan <bhadmuslekan@gmail.com>
2023-01-10 11:54:14 -0800

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
-"""Write a function that reads a text file 
+"""Write a function that reads a text file
    (UTF8) and prints it to stdout
 """
+
+
 def read_file(filename=""):
+
+    """Read a file"""
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
         print(read_data)
