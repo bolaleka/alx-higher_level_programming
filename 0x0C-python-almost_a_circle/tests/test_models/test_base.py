ce6046f8eca32223a3b83d9ddf1edcfa0b1a2a26
olalekan <bhadmuslekan@gmail.com>
2023-01-14 00:45:00 -0800

    0x0C. Python - Almost a circle

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,11 @@
+import sys
+
+sys.path.insert(0, '/root/alx-higher_level_programming/0x0C-python-almost_a_circle/models')
+import unittest
+
+class TestBase(unittest.TestCase):
+    def test_base(self):
+          self.assertTrue(True)
+
+if __name__ == '__main__':
+     unittest.main()
