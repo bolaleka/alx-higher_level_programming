f2a63b34f94d6a9f97304e7751d6c35fe02fbf60
olalekan <bhadmuslekan@gmail.com>
2023-03-14 16:23:35 -0700

    javascript object

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      w = {};
+      h = {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      w = {};
+      h = {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    let str = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        str += 'x';
+      }
+      if (i != this.height - 1) {
+        str += '\n';
+      }
+    }
+    console.log(str);
+  }
+};
