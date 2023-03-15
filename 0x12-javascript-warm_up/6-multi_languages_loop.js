5b24aad13eb4d7c0e570b0d7c34321e18d718130
olalekan <bhadmuslekan@gmail.com>
2023-03-15 05:44:12 -0700

    bug fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arr = ['C is fun', 'Pyhton is cool', 'JavaScript is cool'];
+const arr = ['C is fun', 'Pyhton is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < 3; i++) {
   console.log(arr[i]);
