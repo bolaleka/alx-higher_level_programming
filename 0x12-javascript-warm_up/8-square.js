1b4ff8a17d77c5535e097cfaf91974ebafc44081
olalekan <bhadmuslekan@gmail.com>
2023-03-15 06:41:41 -0700

    bug fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@
 const num = parseInt(process.argv[2]);
 let arr = '';
 
-iif (/\d/.test(num) && process.argv[2] !== undefined && num > 0) {
+if (/\d/.test(num) && process.argv[2] !== undefined && num > 0) {
   for (let i = 0; i < num; i++) {
     for (let j = 0; j < num; j++) {
       arr += 'X';
