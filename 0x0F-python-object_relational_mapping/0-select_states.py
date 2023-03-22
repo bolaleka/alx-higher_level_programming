af8de974b16b20d28280600b5c3c6f4481fe7088
olalekan <bhadmuslekan@gmail.com>
2023-03-22 01:48:47 -0700

    mySQLDB database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,15 +3,15 @@
 
 import MySQLdb
 
-db = MySQLdb.connect(user='root', passwd='root', db='hbtn_0e_0_usa');
-cur = db.cursor();
+db = MySQLdb.connect(user='root', passwd='root', db='hbtn_0e_0_usa')
+cur = db.cursor()
 
 fullLine = ''
 for line in open('0-select_states.sql'):
     tmpLine = line.strip()
 
     if tmpLine[0] == '#' or tmpLine[0] == '-':
-        continue    
+        continue
     fullLine += line
 my_list = [x + ';' for x in fullLine.split(';') if x]
 for item in range(len(my_list) - 1):
@@ -19,7 +19,6 @@ for item in range(len(my_list) - 1):
 cur.execute("SELECT * FROM states")
 rows = cur.fetchall()
 for j in range(len(my_list)):
-    print(rows[j]) 
+    print(rows[j])
 cur.close()
 db.close()
-    
