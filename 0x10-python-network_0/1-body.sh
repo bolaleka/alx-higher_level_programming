475771be2f71af0504edf21ea15d64fa5ebeac6d
olalekan <bhadmuslekan@gmail.com>
2023-03-30 10:22:33 -0700

    Network basic

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to the URL and display the response body
-curl -s -o /dev/null -w "%{http_code}" "$1"
+curl -sL "$1" -X GET -D ./header -o ./output; if grep -q "200 OK" ./header; then cat ./output; fi
