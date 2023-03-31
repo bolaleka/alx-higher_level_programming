d99a317cbefea2761ea70788b71323ab673378ea
olalekan <bhadmuslekan@gmail.com>
2023-03-31 00:03:27 -0700

    javascript object

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -14,7 +14,7 @@ module.exports = class Rectangle {
     let str = '';
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        str += 'x';
+        str += 'X';
       }
       if (i != this.height - 1) {
         str += '\n';
