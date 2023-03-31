e163505606819e923c7fee953819b2111ae5cfa8
olalekan <bhadmuslekan@gmail.com>
2023-03-31 11:37:39 -0700

    Python Network

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,7 +8,8 @@
 import requests
 import sys
 
-url = sys.argv[1]
-response = requests.get(url)
-x_request_id = response.headers.get('X-Request-Id')
-print(x_request_id)
+if __name__ == '__main__':
+    url = sys.argv[1]
+    response = requests.get(url)
+    x_request_id = response.headers.get('X-Request-Id')
+    print(x_request_id)
